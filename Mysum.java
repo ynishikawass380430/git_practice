18e13c7a6d105859d47fc95f26eb120994f943e7
ynishikawa <ynishikawa@crooz.co.jp>
2015-11-30 18:47:17 +0900

    initial commit

---
+++ Mysum.java
@@ -0,0 +1,14 @@
+﻿public class Main { 
+  public static void main(String[] args){
+    System.out.println("Hello World");
+
+int a;
+int b;
+int c;
+int d;
+a =1;
+b =4;
+c = a+b;
+System.out.println("計算結果:"+c);  
+ }
+}
